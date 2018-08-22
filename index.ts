29768015c8e00f8472baef6710111210c3d04cc8
Dmytro Horbatko <gorbatko.dv@gbksoft.com>
2018-08-22 18:18:48 +0300

    fix number of games and compile options

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import * as child from 'child_process';
 import * as os from 'os';
 import * as fs from "fs";
 
-const NUMBER_OF_GAMES = 100000000;
+const NUMBER_OF_GAMES = 1000000000;
 const slotWorker = './models/tt-game.ts';
 
 const numOfCpus = os.cpus().length;
-const params: ReadonlyArray<string> = [`--numbersOfGames=${NUMBER_OF_GAMES}`, `--bet=1`];
+const numOfGamesPerCPU = NUMBER_OF_GAMES / (numOfCpus || 1);
+const params: ReadonlyArray<string> = [`--numbersOfGames=${numOfGamesPerCPU}`, `--bet=1`];
 
 const games = {wins: 0, loses: 0};
 const startTime = new Date();
@@ -22,8 +23,8 @@ if (process.execArgv.toString().indexOf('--inspect-brk') !== -1) {
 for (let ind = 0; ind < numOfCpus; ind++) {
     const inst = child.fork(slotWorker, params)
         .on('exit', res => {
-            console.log(`Games ${ind} have been finished at ${new Date()}`);
-            fs.appendFile('./output/result.log', `Games ${ind} have been finished at ${new Date()}\n`, errorHandler);
+            console.log(`Games ${ind} have been finished  with ${numOfGamesPerCPU} games at ${new Date()}`);
+            fs.appendFile('./output/result.log', `Games ${ind} have been finished with ${numOfGamesPerCPU} games at ${new Date()}\n`, errorHandler);
         })
         .on('message', msg => {
             if (msg.loses) {
